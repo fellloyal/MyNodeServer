cd8632d9f0e93a126464a13adafc9d8e3e7eccf6
fellloyal <fellloyal@163.com>
2018-07-18 15:33:48 +0800

    日志文件名按日生成

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var mqtt = require('mqtt'); //生成MQTT对象
 
 var log4js = require('log4js');//生成日志对象
 
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient; //生成MongoDb对象
 
 var url  ="mongodb://127.0.0.1"
-var dbo ;
+var dbo ;//数据库连接对象
 MongoClient.connect(url,function(err,db){
   if(err) throw err;
   console.log("mongodb 数据库连接成功");
@@ -15,13 +15,19 @@ MongoClient.connect(url,function(err,db){
 });
 
 
+//配置日志输出
 log4js.configure({
     appenders: {
-      out: { type: 'stdout' },//设置是否在控制台打印日志
-      info: { type: 'file', filename: './logs/info.log' },
       
-    },
-    categories: {
+      out: { type: 'stdout' },//设置是否在控制台打印日志
+      info: {type: 'dateFile',
+        filename: 'logs/log',
+        pattern: '_yyyy-MM-dd.log',
+        alwaysIncludePattern: true,
+        category: 'dateFileLog'      },
+      },
+
+        categories: {
       default: { appenders: [ 'out', 'info' ], level: 'info' }//去掉'out'。控制台不打印日志
     }
   });
@@ -29,16 +35,21 @@ log4js.configure({
   var logger = log4js.getLogger('info'); 
 
   
-
+//mqtt连接对象
 var client  = mqtt.connect('mqtt://127.0.0.1');
 
-client.on('connect', function () {
+function ConnectMqttHandler()
+{
   client.subscribe('OilServer/#')
   //client.publish('oilPrice', 'Hello mqtt')
   logger.info('Server Connected');
   console.log("mqtt 服务器连接成功") 
-})
+}
+//定义连接mqtt服务器事件
+client.on('connect',  ConnectMqttHandler());
 
+
+//收到mqtt 消息事件
 client.on('message', function (topic, message) {
   // message is Buffer
 
