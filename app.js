09478c840d0f579ce5b8cf51db66bcbdca47d99e
fellloyal <fellloyal@163.com>
2018-07-19 22:59:12 +0800

    加入promise,未解决返回结果问题

--- app.js
+++ app.js
@@ -7,8 +7,18 @@ var MongoClient = require('mongodb').MongoClient; //生成MongoDb对象
 
 var mysql = require('mysql');//生成mysql对象
 
+var fs = require('fs');//得到file对象
 
 
+
+var mysqlConnection;//声明mysql连接对象
+
+var configData;//声明系统配置文件对象
+
+
+var dbo;//mongoDb数据库连接对象
+
+//------从日志开始初始化------使用promise语法
 //配置日志输出
 log4js.configure({
   replaceConsole: true,
@@ -34,38 +44,74 @@ var logger = log4js.getLogger('info');
 logger.info("------------------启动服务程序--------------------------")
 
 
-var fs = require('fs');
-fs.readFile("./appConfig.json", function (err, filedata) {
-  filedata = JSON.parse(filedata);
-  logger.info(filedata.mongoDbConfig.mongoDbServerIp);
+
+
+var promiseCodes = new Promise((resolve, reject) => {
+
+  configData = fs.readFileSync("./appConfig.json");
+
+  resolve('ok');
+
+})
+
+promiseCodes.then((value) => {
+  configData = JSON.parse(configData);
   
-}
-);
+})
+  .then((value) => {
 
+    mysqlConnection = mysql.createConnection({
+      /*
+      host: 'localhost',
+      user: 'root',
+      password: 'root',
+      database: 'OilCloudDB'
+      */
+      host: configData.mysqlConfig.mysqlServerUrl,
+      user: configData.mysqlConfig.mysqlUserName,
+      password: configData.mysqlConfig.mysqlPassword,
+      database: configData.mysqlConfig.mysqlDataBaseName
+    });
+    return 0;
+  })
 
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'OilCloudDB'
-});
+  .then((value) => {
 
-connection.connect(function () {
-  logger.info("mysql 数据库连接成功");
+    mysqlConnection.connect(function () {
+      logger.info("mysql server url : " + configData.mysqlConfig.mysqlServerUrl, );
+      logger.info("mysql 数据库名 : " + configData.mysqlConfig.mysqlDataBaseName);
 
-  connection.query('SELECT * from oc_oiltrade limit 1', function (error, results, fields) {
-    if (error) throw error;
-    logger.info(results);
+      logger.info("mysql 数据库连接成功");
 
+      mysqlConnection.query('SELECT * from oc_oiltrade limit 1', function (error, results, fields) {
 
+        logger.info(results);
 
+        return 0;
+
+      });
+
+    });
+
+  })
+  .then((value)=>{
+    logger.info("mongoDb server url : "+configData.mongoDbConfig.mongoDbServerUrl);
+    logger.info("mongoDb 数据集名 : "+configData.mongoDbConfig.mongoCollectionName);
+    
+    MongoClient.connect(configData.mongoDbConfig.mongoDbServerUrl);
+  })
+  .then((value)=>{
+    logger.info("mongodb 服务器连接成功");
+  })
+  
+  .catch(function (error) {
+    logger.info(error);
   });
 
-});
 
 
-var url = "mongodb://127.0.0.1"
-var dbo;//数据库连接对象
+
+
 MongoClient.connect(url, function (err, db) {
   if (err) throw err;
   logger.info("mongodb 数据库连接成功");
@@ -98,7 +144,7 @@ client.on('message', function (topic, message) {
   // message is Buffer
 
   console.log(topic.toString());
-  console.log(message.toString());
+  console.log(message.toString("utf8"));
   logger.info('Server get Message');
   client.publish('Client/PC0000001', 'Server Answer');
   var myobj = { topic: topic, message: message.toString() };
@@ -109,9 +155,9 @@ client.on('message', function (topic, message) {
   // client.end();
 })
 
-process.on('SIGINT', function() {
+process.on('SIGINT', function () {
   logger.info("进程收到 SIGINT 信号 准备退出!");
-   
+
   process.exit(0);
 });
 
